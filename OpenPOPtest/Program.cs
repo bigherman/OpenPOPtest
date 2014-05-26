cd367e431c006341a2c8ab5b4804c19b0c077e5e
bigherman <pasa@mercantec.dk>
2014-05-26 21:00:11 +0200

    initial commit

---
+++ OpenPOPtest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenPop.Pop3;
+
+namespace OpenPOPtest
+{
+    class Program
+    {
+        public int test
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        static void Main(string[] args)
+        {
+            Pop3Client client = new Pop3Client();
+
+            client.Connect("mail1.stofanet.dk", 110, false);
+            client.Authenticate("2241859m001", "");
+            for (int messageNo = 1; messageNo < client.GetMessageCount() + 1; messageNo++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(client.GetMessageHeaders(messageNo).MessageId);
+                Console.WriteLine("Subject:");
+                Console.WriteLine(client.GetMessageHeaders(messageNo).Subject);
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Body:");
+                    Console.WriteLine(client.GetMessage(messageNo).FindFirstPlainTextVersion().GetBodyAsText());
+                }
+                catch
+                {
+                }
+                Console.WriteLine("|---------------------------------------------------------------------------------------------------------------------|");
+            }
+        }
+    }
+}
